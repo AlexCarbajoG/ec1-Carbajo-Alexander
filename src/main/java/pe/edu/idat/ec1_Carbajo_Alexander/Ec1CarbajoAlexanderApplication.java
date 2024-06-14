c154b92a4f0cfbeed47cb3fa24709033afdaeb9b
AJcarbajoG <alexander.carbajogrados@gmail.com>
2024-06-13 21:41:23 -0500

    primer commit

---
+++ src/main/java/pe/edu/idat/ec1_Carbajo_Alexander/Ec1CarbajoAlexanderApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec1_Carbajo_Alexander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ec1CarbajoAlexanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ec1CarbajoAlexanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/idat/ec1_Carbajo_Alexander/Ec1CarbajoAlexanderApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.ec1_Carbajo_Alexander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec1CarbajoAlexanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
